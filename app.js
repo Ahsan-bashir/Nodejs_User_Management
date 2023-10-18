eee5351e964a6ef2ccb6f864736660fd5277ed95
Ahsan-bashir <bteeccc@gmail.com>
2023-10-18 21:19:25 +0500

    Project setup for frontend using bootstrap ejs

---
+++ app.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+
+const express = require('express')
+const expressLayout = require('express-ejs-layouts')
+
+const app = express()
+const port = process.env.PORT || 5000
+
+//To pass data through forms
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static Files
+app.use(express.static('public'));
+ 
+// Set Templating Engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+// Home 
+app.get('/',(req,res)=>{
+    const locals={
+        title:"Nodejs",
+        description:"Nodejs User Management System"
+    }
+    res.render('index',locals);
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})
